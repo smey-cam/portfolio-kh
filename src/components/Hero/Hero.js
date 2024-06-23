674fdc1681595802c99ee8a178aa077e19bcf737
Chham Dararaksmey <58686050+chhamgit@users.noreply.github.com>
2024-06-23 20:07:56 +0700

    fix warning

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,8 +12,8 @@ const Hero = (props) => (
           Allow me to introduce myself
         </SectionTitle>
         <SectionText>
-          I'm Chham Dararaksmey, Have over 3 years of experience in maintaining existing applications or creating one from scratch.
-          Lives by "Attitude is everything." and always "100% in everything I do."
+          I am Chham Dararaksmey, Have over 3 years of experience in maintaining existing applications or creating one from scratch.
+          Lives by Attitude is everything. and always 100% in everything I do.
           Constantly strive to learn new things and seek advices for improvement in everything.
           Passionate in coding, especially when it involves creating applications that help others to achieve success.
           Graduated from RUPP University with a bachelor degree of B.S. in Computer Science.
--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>My Journey</SectionTitle>
       <SectionText>
-      Throughout my journey, I've gained a wealth of knowledge and experience in both development and life in general. I’m always eager to learn more and take on new challenges,
+      Throughout my journey, I have gained a wealth of knowledge and experience in both development and life in general. I’m always eager to learn more and take on new challenges,
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>

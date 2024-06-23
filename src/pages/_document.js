416f5a68dc49e7c052176e9e9bf473269acc125b
Chham Dararaksmey <58686050+chhamgit@users.noreply.github.com>
2024-06-23 20:19:44 +0700

    change descritpion

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,14 +33,14 @@ export default class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
           <meta name="title" content="Chham Dararaksmey - Web Developer" />
-          <meta name="description" content="Chham Dararaksmey, also known as lordarcadius is an Android Engineer at Primebook. He has also worked on ROMs, Kernels and Scripts." />
+          <meta name="description" content="I am Chham Dararaksmey, Have over 3 years of experience in maintaining existing applications or creating one from scratch. Lives by Attitude is everything. and always 100% in everything I do. Constantly strive to learn new things and seek advices for improvement in everything. Passionate in coding, especially when it involves creating applications that help others to achieve success. Graduated from RUPP University with a bachelor degree of B.S. in Computer Science. Feel free to contact me if you need my help or know more about me!" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Chham Dararaksmey - Web Developer" />
-          <meta property="og:description" content="Chham Dararaksmey, also known as lordarcadius is an Android Engineer at Primebook. He has also worked on ROMs, Kernels and Scripts." />
-          <meta property="og:image" content="https://www.vipuljha.com/images/banner.png" />
-          <meta property="twitter:card" content="summary_large_image" />
+          {/* <meta property="og:description" content="Chham Dararaksmey, also known as lordarcadius is an Android Engineer at Primebook. He has also worked on ROMs, Kernels and Scripts." /> */}
+          {/* <meta property="og:image" content="https://www.vipuljha.com/images/banner.png" /> */}
+          {/* <meta property="twitter:card" content="summary_large_image" /> */}
           <meta property="twitter:title" content="Chham Dararaksmey - Web Developer" />
-          <meta property="twitter:description" content="Chham Dararaksmey, also known as lordarcadius is an Android Engineer at Primebook. He has also worked on ROMs, Kernels and Scripts." />
+          {/* <meta property="twitter:description" content="Chham Dararaksmey, also known as lordarcadius is an Android Engineer at Primebook. He has also worked on ROMs, Kernels and Scripts." /> */}
           {/* <meta property="twitter:image" content="https://www.vipuljha.com/images/banner.png" /> */}
         </Head>
         <body>
